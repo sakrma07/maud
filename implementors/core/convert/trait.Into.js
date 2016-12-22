fce9702706d31258db3edd573e16ec0d4587ae1f
Chris Wong <lambda.fairy@gmail.com>
2016-12-22 20:09:13 +1300

    Update documentation

---
+++ implementors/core/convert/trait.Into.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["maud"] = ["impl&lt;T:&nbsp;<a class='trait' href='https://doc.rust-lang.org/nightly/core/convert/trait.AsRef.html' title='core::convert::AsRef'>AsRef</a>&lt;<a class='primitive' href='https://doc.rust-lang.org/nightly/std/primitive.str.html'>str</a>&gt; + <a class='trait' href='https://doc.rust-lang.org/nightly/core/convert/trait.Into.html' title='core::convert::Into'>Into</a>&lt;<a class='struct' href='https://doc.rust-lang.org/nightly/collections/string/struct.String.html' title='collections::string::String'>String</a>&gt;&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/convert/trait.Into.html' title='core::convert::Into'>Into</a>&lt;<a class='struct' href='https://doc.rust-lang.org/nightly/collections/string/struct.String.html' title='collections::string::String'>String</a>&gt; for <a class='struct' href='maud/struct.PreEscaped.html' title='maud::PreEscaped'>PreEscaped</a>&lt;T&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ implementors/iron/response/trait.WriteBody.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["maud"] = ["impl <a class='trait' href='http://ironframework.io/core/iron/iron/response/trait.WriteBody.html' title='iron::response::WriteBody'>WriteBody</a> for <a class='struct' href='maud/struct.PreEscaped.html' title='maud::PreEscaped'>PreEscaped</a>&lt;<a class='struct' href='https://doc.rust-lang.org/nightly/collections/string/struct.String.html' title='collections::string::String'>String</a>&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
